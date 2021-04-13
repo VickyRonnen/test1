2c5e35f88f02b7603834f3e00582ee5e46eff95e
Vicky Ronnen <v.ronnen@gmail.com>
2021-04-13 18:29:19 +0200

    initial commit

---
+++ src/test/java/nl/denkzelf/Test1.java
@@ -0,0 +1,21 @@
+package nl.denkzelf;
+
+import nl.denkzelf.test2.Tester2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+  @Test
+  void test1() {
+    String s = Tester2.myStaticHelper();
+    Assertions.assertEquals("this helps static",Tester2.myStaticHelper());
+  }
+
+  @Test
+  void test2() {
+    Tester2 tester2 = new Tester2();
+    Assertions.assertEquals("this helps",tester2.myHelper());
+  }
+
+}
